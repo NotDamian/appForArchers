17a6e37188d3061441c30b6431837c1ffa3a5e5a
NotDamian <60854998+NotDamian@users.noreply.github.com>
2023-02-10 11:41:29 +0100

    Update _document.js

--- app-for-archers/pages/_document.js
+++ app-for-archers/pages/_document.js
@@ -7,7 +7,7 @@ export default function Document() {
       <Head>
         <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600&display=swap" rel="stylesheet" />
         <link rel="icon" href="../public/favicon.ico" />
-        <Script src="https://kit.fontawesome.com/4c93254228.js" crossorigin="anonymous" />
+        <script src="https://kit.fontawesome.com/4c93254228.js" crossorigin="anonymous"></script>
       </Head>
       {/* <Head /> */}
       <body>
